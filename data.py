5223d86854dc82ee13b3dd7582ab9d0250708220
guaiguaimeimei <1436176195@qq.com>
2025-07-12 07:19:57 +0800

    read

---
+++ data.py
@@ -0,0 +1,6 @@
+class Record:
+    def __init__(self,data,id,money,add):
+        self.data=data
+        self.id=id
+        self.money=money
+        self.add=add
---
+++ read.py
@@ -0,0 +1,2 @@
+from data import Record
+class Filereader: ->list[Record]
