790416966353459207d7b292ee7142cf5a35b757
guaiguaimeimei <1436176195@qq.com>
2025-07-12 15:49:36 +0800

    Update read.py

--- read.py
+++ read.py
@@ -9,6 +9,12 @@ class TextFilereader(Filereader):
     #实现父类
     def read_date(self->list[Record]:
         f=open(self.path,"r",encoding="UTF-8")
+        record_list:list[Record]=[]
         for line in f.redalines():
             line=line.sterp():
-            line.split(",")
+            data_list=line.split(",")
+            record=Record(data_list[0],data_lisr[1],imt(data_lisr[2]),data_list[3])
+            record_list.append(record)
+         f.close() 
+        return record_list
+        
