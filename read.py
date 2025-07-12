26ac3aa36d95d428685b4901e20af018c0cefa49
guaiguaimeimei <1436176195@qq.com>
2025-07-12 16:13:41 +0800

    Update read.py

--- read.py
+++ read.py
@@ -17,4 +17,16 @@ class TextFilereader(Filereader):
             record_list.append(record)
          f.close() 
         return record_list
+class JsonFilereader(Filereader):
+    def __init__(self,path):
+        self.puth=path
         
+    def read_date(self->list[Record]:
+        f=open(self.path,"r",encoding="UTF-8")
+        record_list:list[Record]=[]
+        for line in f.redalines():
+            data_dict=json.loads(line)
+            record=Record(data_dict["date"],data_dict["order_id"],int(data_dice["amount"]),data_dict["city"])
+            record_list.append(record)
+         f.close() 
+        return record_list
