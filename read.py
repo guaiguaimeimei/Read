2878cfe8861d1ce48c0c30a4f4943974d022c1a3
guaiguaimeimei <1436176195@qq.com>
2025-07-12 15:39:33 +0800

    Update read.py

--- read.py
+++ read.py
@@ -1,6 +1,14 @@
 from data import Record
 class Filereader: 
-  def read_date(self->list[Record]:
-    pass
+    def read_date(self->list[Record]:
+        pass
 class TextFilereader(Filereader):
-  
+    def __init__(self,path):
+        #定义文件路径
+        self.path=path
+    #实现父类
+    def read_date(self->list[Record]:
+        f=open(self.path,"r",encoding="UTF-8")
+        for line in f.redalines():
+            line=line.sterp():
+            line.split(",")
